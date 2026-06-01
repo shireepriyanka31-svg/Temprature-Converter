226a610a3fb03157aa8554b6937ee845469d0a74
IshaaqAV <ishaaq.vasim@gmail.com>
2026-06-01 09:18:04 +0530

    Added option to select between Celcius, Fahrenheit, Kelvin scal to be converted

--- main.c
+++ main.c
@@ -4,11 +4,43 @@
 int main()
 {
     float C,F,K;
-    printf("Enter the Temprature(in Celsius): ",C);
+    char opt;
+
+    printf("Choose Convertion Format\nOption a: C to F/K\nOption b: F to C/K\nOption c: K to C/F\n\nOption: ");
+    scanf(" %c", &opt);
+
+    if (opt=='a'){
+            printf("Enter the Temperature(in Celsius): ",C);
+            scanf("%f",&C);
+            F=(C*9/5)+32;
+            K=C+273;
+            printf("In Fahrenheit: %.2f°F \n",F);
+            printf("In Kelvin: %.2f K",K);
+    }
+    else if (opt=='b'){
+            printf("Enter the Temperature(in Fahrenheit): ",C);
+            scanf("%f",&C);
+            F=(C*9/5)+32;
+            K=C+273;
+            printf("In Fahrenheit: %.2f°F \n",F);
+            printf("In Kelvin: %.2f K",K);    }
+    else if (opt=='c'){
+            printf("Enter the Temperature(in Kelvin): ",C);
+            scanf("%f",&C);
+            F=(C*9/5)+32;
+            K=C+273;
+            printf("In Fahrenheit: %.2f°F \n",F);
+            printf("In Kelvin: %.2f K",K);    }
+    else {
+        printf("Error: Wrong input\nTry again");
+
+
+    printf("Enter the Temperature(in Celsius): ",C);
     scanf("%f",&C);
     F=(C*9/5)+32;
     K=C+273;
     printf("In Fahrenheit: %.2f°F \n",F);
     printf("In Kelvin: %.2f K",K);
+    }
     return 0;
 }
